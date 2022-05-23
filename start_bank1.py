9aa2bfbf64e478d8c252b5da5a89c65185218d4a
Gadir Rustamli <gadir.rustamli@outlook.com>
2022-05-23 12:17:58 +0400

    Update start_bank1.py

--- start_bank1.py
+++ start_bank1.py
@@ -3,8 +3,7 @@ import globals
 
 
 def start_bank():
-    # bank1 = bank.Bank(globals.BANK1_ID, clients={globals.TEST_CLIENT_ID: globals.BANK1_EXISTING_SECRET})
-    bank1 = bank.Bank(globals.BANK1_ID)
+    bank1 = bank.Bank(globals.BANK1_ID, clients={globals.TEST_CLIENT_ID: globals.BANK1_EXISTING_SECRET})
     bank1.start()
 
 if __name__ == '__main__':
